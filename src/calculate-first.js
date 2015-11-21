cfa2f2b0c1f60a299eab166db023c9d2102a9737
nani <fengwu666@gmail.com>
2015-11-21 22:05:19 +0800

    simple test

--- src/calculate-first.js
+++ src/calculate-first.js
@@ -1,3 +1,5 @@
 function single_element(arr) {
-  
+	ret = [];
+	ret = arr.filter(function(value,ret){if(ret%2==1) return true}).filter(function(value,index,arr1){if(arr1.indexOf(value)==arr1.lastIndexOf(value))return true;else return false;})
+	return ret;
 }
